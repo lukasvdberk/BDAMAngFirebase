ff7fd77eb985bec6d20ef3d86ea89aa34f0c99b3
KeithMarex <koenvandermarel@gmail.com>
2021-01-11 00:41:02 +0100

    Niet meer nodig, foutje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import {fas} from '@fortawesome/free-solid-svg-icons';
 import {far} from '@fortawesome/free-regular-svg-icons';
 import { CountdownComponent } from './countdown/countdown.component';
 import { DashboardComponent } from './user-portal/dashboard/dashboard.component';
-import { SortByPipe } from './shared/sort-by.pipe';
 
 const appRoutes: Routes = [
   {path: 'admin', component: UserPortalComponent},
@@ -32,7 +31,6 @@ const appRoutes: Routes = [
     CountdownComponent,
     CountdownComponent,
     DashboardComponent,
-    SortByPipe,
   ],
   imports: [
     BrowserModule,
